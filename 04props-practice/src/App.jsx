f7164ce0479b0c0f9c1a2f8a665fb0de24bcdc5c
arkaslittlemind <arkadiptamojumder14@gmail.com>
2024-07-18 19:31:07 +0530

    practicing passing props

---
+++ 04props-practice/src/App.jsx
@@ -0,0 +1,18 @@
+import Card from './components/Card'
+
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Card btnText = "Read Me!"/>
+      <Card btnText = "Click Me!"/>
+      <Card />
+    </>
+  )
+}
+
+export default App
