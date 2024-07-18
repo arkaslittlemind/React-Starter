a25875c96f3349b1370a2a075d208abba6ca2d7d
arkaslittlemind <arkadiptamojumder14@gmail.com>
2024-07-19 02:04:06 +0530

    component placed in the center

--- 06passwordGenerator/src/App.jsx
+++ 06passwordGenerator/src/App.jsx
@@ -1,7 +1,11 @@
 import PasswordGenerator from "./components/PasswordGenerator";
 
 function App() {
-  return <PasswordGenerator />;
+  return (
+    <div className="h-screen w-full flex items-center justify-center">
+      <PasswordGenerator />
+    </div>
+  );
 }
 
 export default App;
