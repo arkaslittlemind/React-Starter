fd18e77656d580163e3bba87b61e1ef65014f283
arkaslittlemind <arkadiptamojumder14@gmail.com>
2024-07-25 17:21:05 +0530

    needs bug fixes on App.jsx

--- 07currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 07currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -4,7 +4,7 @@ function useCurrencyInfo(currency) {
     const [data, setData] = useState({});
 
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/inr.json`)
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json`)
         .then((response) => response.json())
         .then((response) => setData(response[currency]))
         console.log(data);
