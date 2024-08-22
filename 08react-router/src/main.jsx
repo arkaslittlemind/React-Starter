e0925b11b4465e1a57d93b9106852ddd80c73323
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2024-08-23 01:33:02 +0530

    refactor: Update Home component with download button and images

--- 08react-router/src/main.jsx
+++ 08react-router/src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout'
+import Home from './components/Home/Home'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {}
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
