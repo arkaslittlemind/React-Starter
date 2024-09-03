9bafcfc03bc4903adf83652f7918b331e4127234
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2024-09-04 02:05:46 +0530

    Context API init

---
+++ 09contextAPI/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-3xl bg-green-600 text-white rounded-md hover:bg-green-800'>Hello from Context API</h1>
+    </>
+  )
+}
+
+export default App
