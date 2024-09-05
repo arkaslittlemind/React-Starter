1b785c54d7123c8f66c4711b32b0bbb0fa34af5b
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2024-09-06 02:51:57 +0530

    feat: local storage done

--- 11contextTodo/src/App.jsx
+++ 11contextTodo/src/App.jsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { TodoProvider } from "./contexts/TodoContext";
+import TodoItem from "./components/TodoItem";
+import TodoForm from "./components/TodoForm";
 
 
 function App() {
@@ -27,6 +29,18 @@ function App() {
     )))
   }
 
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+
+    if(todos && todos.length > 0) {
+      setTodos(todos);;
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   return (
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleTodo}}>
     <div className="bg-[#172842] min-h-screen py-8">
@@ -36,9 +50,15 @@ function App() {
         </h1>
         <div className="mb-4">
           {/* Todo form goes here */}
+          <TodoForm />
         </div>
         <div className="flex flex-wrap gap-y-3">
           {/*Loop and Add TodoItem here */}
+          {todos.map((todo) => (
+            <div key={todo.id} className="w-full">
+              <TodoItem todo={todo} />
+            </div>
+          ))}
         </div>
       </div>
     </div>
