05918ca970a0b83a8ac19ac085a2df3908e1992b
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2024-09-06 03:00:38 +0530

    chore: added responsiveness

--- 11contextTodo/src/components/TodoForm.jsx
+++ 11contextTodo/src/components/TodoForm.jsx
@@ -17,20 +17,19 @@ function TodoForm() {
   }
 
   return (
-      <form  onSubmit={handleSubmit} className="flex">
-          <input
-              type="text"
-              placeholder="Write Todo..."
-              className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
-              value={todo}
-              onChange={(e) => setTodo(e.target.value)}
-          />
-          <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
-              Add
-          </button>
-      </form>
+    <form onSubmit={handleSubmit} className="flex w-full max-w-md mx-auto sm:max-w-lg lg:max-w-2xl p-2 sm:p-4">
+      <input
+        type="text"
+        placeholder="Write Todo..."
+        className="flex-1 border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-2 sm:py-3"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button type="submit" className="rounded-r-lg px-3 py-2 sm:py-3 bg-green-600 text-white shrink-0">
+        Add
+      </button>
+    </form>
   );
 }
 
 export default TodoForm;
-
